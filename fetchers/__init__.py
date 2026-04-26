6c8079a9dfadbe6b8e52fa2e3a8f937cb3002367
Light <lightman.chang@gmail.com>
2026-04-26 11:06:51 +0800

    feat: add Playwright fetchers for all tracked companies

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -1,6 +1,43 @@
 """
-Company fetcher registry.
-Add Playwright-based fetchers here as they are implemented.
+公司新聞爬蟲
 """
 
-FETCHERS = {}
+from .base import CompanyFetcher, CompanyDocument
+
+from .albemarle import AlbemarleFetcher
+from .basf import BasfFetcher
+from .byd import BydFetcher
+from .catl import CatlFetcher
+from .eve import EveFetcher
+from .ganfeng import GanfengFetcher
+from .lg_energy import LgEnergyFetcher
+from .nio import NioFetcher
+from .panasonic import PanasonicFetcher
+from .pilbara import PilbaraFetcher
+from .rivian import RivianFetcher
+from .samsung_sdi import SamsungSdiFetcher
+from .sk_on import SkOnFetcher
+from .sqm import SqmFetcher
+from .tesla import TeslaFetcher
+from .umicore import UmicoreFetcher
+from .volkswagen import VolkswagenFetcher
+
+FETCHERS = {
+    "albemarle": AlbemarleFetcher,
+    "basf": BasfFetcher,
+    "byd": BydFetcher,
+    "catl": CatlFetcher,
+    "eve": EveFetcher,
+    "ganfeng": GanfengFetcher,
+    "lg_energy": LgEnergyFetcher,
+    "nio": NioFetcher,
+    "panasonic": PanasonicFetcher,
+    "pilbara": PilbaraFetcher,
+    "rivian": RivianFetcher,
+    "samsung_sdi": SamsungSdiFetcher,
+    "sk_on": SkOnFetcher,
+    "sqm": SqmFetcher,
+    "tesla": TeslaFetcher,
+    "umicore": UmicoreFetcher,
+    "volkswagen": VolkswagenFetcher,
+}
